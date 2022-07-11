a208f62afa2fd4a35807d809619a58f802304c2d
LeeJin0527 <ijin17463@gmail.com>
2022-07-11 20:05:56 +0900

    [Setting] initial setting

---
+++ src/main/java/swm/wbj/asyncrum/AsyncrumApplication.java
@@ -0,0 +1,13 @@
+package swm.wbj.asyncrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncrumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swm/wbj/asyncrum/AsyncrumApplicationTests.java
@@ -0,0 +1,14 @@
+package swm.wbj.asyncrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hello");
+	}
+
+}
