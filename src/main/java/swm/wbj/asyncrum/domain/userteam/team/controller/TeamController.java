799412bc8b3ddd3b9a33cdb96a6aaaa92e7fc1aa
Kevin Park <wlghks0314@gmail.com>
2022-07-12 22:20:17 +0900

    [Feature] Team domain CRUD logic implemented

--- src/main/java/swm/wbj/asyncrum/domain/userteam/team/controller/TeamController.java
+++ src/main/java/swm/wbj/asyncrum/domain/userteam/team/controller/TeamController.java
@@ -1,4 +1,76 @@
 package swm.wbj.asyncrum.domain.userteam.team.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import swm.wbj.asyncrum.domain.userteam.team.dto.*;
+import swm.wbj.asyncrum.domain.userteam.team.service.TeamService;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/teams")
+@RestController
 public class TeamController {
+    private final TeamService teamService;
+
+    @PostMapping
+    public ResponseEntity<?> createTeam(@RequestBody TeamCreateRequestDto requestDto) {
+        try {
+            Long createdId = teamService.createTeam(requestDto);
+
+            return ResponseEntity.ok(createdId);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> readTeam(@PathVariable Long id) {
+        try {
+            TeamReadResponseDto responseDto = teamService.readTeam(id);
+
+            return ResponseEntity.ok(responseDto);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> readAllTeam(@RequestBody TeamReadAllRequestDto requestDto) {
+        try {
+            TeamReadAllResponseDto responseDto = teamService.readAllTeam(requestDto);
+
+            return ResponseEntity.ok(responseDto);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateTeam(@PathVariable Long id, @RequestBody TeamUpdateRequestDto requestDto) {
+        try {
+            Long updateId = teamService.updateTeam(id, requestDto);
+
+            return ResponseEntity.ok(updateId);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTeam(@PathVariable Long id) {
+        try {
+            teamService.deleteTeam(id);
+
+            return ResponseEntity.noContent().build();
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
