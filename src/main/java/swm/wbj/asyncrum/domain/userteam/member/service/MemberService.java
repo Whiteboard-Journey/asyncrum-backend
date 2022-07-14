7770df5359f5bd73ec1d67677167fe5ca9cb0d42
LeeJin0527 <ijin17463@gmail.com>
2022-07-14 11:58:53 +0900

    [Refactor/member-domain] 회원 도메인 리펙토링

--- src/main/java/swm/wbj/asyncrum/domain/userteam/member/service/MemberService.java
+++ src/main/java/swm/wbj/asyncrum/domain/userteam/member/service/MemberService.java
@@ -1,17 +1,14 @@
 package swm.wbj.asyncrum.domain.userteam.member.service;
 
-import swm.wbj.asyncrum.domain.userteam.member.dto.MemberCreateRequestDto;
-import swm.wbj.asyncrum.domain.userteam.member.dto.MemberReadAllResponseDto;
-import swm.wbj.asyncrum.domain.userteam.member.dto.MemberReadResponseDto;
-import swm.wbj.asyncrum.domain.userteam.member.dto.MemberUpdateRequestDto;
+import swm.wbj.asyncrum.domain.userteam.member.dto.*;
 import swm.wbj.asyncrum.domain.userteam.team.dto.TeamReadAllResponseDto;
 import swm.wbj.asyncrum.domain.userteam.team.dto.TeamUpdateRequestDto;
 
 
 public interface MemberService {
-    Long createMember(MemberCreateRequestDto requestDto);
-    MemberReadResponseDto readMember(Long id) throws Exception;
+    MemberCreateResponseDto createMember(MemberCreateRequestDto requestDto);
+    MemberReadResponseDto readMember(Long id);
     MemberReadAllResponseDto readAllMember(Integer pageIndex, Long topId);
-    Long updateMember(Long id, MemberUpdateRequestDto requestDto);
+    MemberUpdateResponseDto updateMember(Long id, MemberUpdateRequestDto requestDto);
     void deleteMember(Long id);
 }
