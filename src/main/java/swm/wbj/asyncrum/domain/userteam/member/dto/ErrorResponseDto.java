70a8c4af5015b1bfa0602c7b1a6d168790f417b5
LeeJin0527 <ijin17463@gmail.com>
2022-07-14 11:58:53 +0900

    [Refactor/member-domain] 회원 도메인 리펙토링

---
+++ src/main/java/swm/wbj/asyncrum/domain/userteam/member/dto/ErrorResponseDto.java
@@ -0,0 +1,10 @@
+package swm.wbj.asyncrum.domain.userteam.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponseDto {
+    private String errorMessage;
+}
