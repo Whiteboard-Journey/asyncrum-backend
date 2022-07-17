53b03765a8a8353c3865115bf1a66dc00a374d40
Kevin Park <wlghks0314@gmail.com>
2022-07-17 20:28:02 +0900

    [chore] remove test code

--- src/test/java/swm/wbj/asyncrum/AsyncrumApplicationTests.java
+++ src/test/java/swm/wbj/asyncrum/AsyncrumApplicationTests.java
@@ -1,14 +1,13 @@
 package swm.wbj.asyncrum;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class AsyncrumApplicationTests {
 
-	@Test
-	void contextLoads() {
-		System.out.println("hello");
-	}
+
 
 }
