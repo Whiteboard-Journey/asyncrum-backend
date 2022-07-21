c694ec35e6ab131d5b1a9aa9c22cc2beb699a94b
Komputer-P <wlghks0314@gmail.com>
2022-07-21 19:31:58 +0900

    WBJ-237 [feature] implement getCurrentMember from JWT - Security Context

--- src/main/java/swm/wbj/asyncrum/domain/userteam/member/service/MemberServiceImpl.java
+++ src/main/java/swm/wbj/asyncrum/domain/userteam/member/service/MemberServiceImpl.java
@@ -12,6 +12,7 @@ import swm.wbj.asyncrum.domain.userteam.member.dto.*;
 import swm.wbj.asyncrum.domain.userteam.member.entity.Member;
 import swm.wbj.asyncrum.domain.userteam.member.repository.MemberRepository;
 import swm.wbj.asyncrum.domain.userteam.team.dto.TeamReadAllResponseDto;
+import swm.wbj.asyncrum.global.oauth.utils.TokenUtil;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +21,33 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public MemberCreateResponseDto createMember(MemberCreateRequestDto requestDto) {
+        Member member = requestDto.toEntity(passwordEncoder);
+        return new MemberCreateResponseDto(memberRepository.save(member).getId());
+    }
+
+    /**
+     * 요청을 보낸 사용자의 정보 가져오기
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public Member getCurrentMember() {
+        // JWT 토큰 -> Security Context의 Authenication -> Member id -> Member 엔티티 가져오기
+        Long memberId = TokenUtil.getCurrentMemberId();
+
+        return memberRepository.findById(memberId).orElseThrow(() -> new IllegalArgumentException("해당 멤버가 존재하지 않습니다."));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public MemberReadResponseDto readMember(Long id){
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 멤버가 존재하지 않습니다."));
+
+        return new MemberReadResponseDto(member);
+    }
 
     @Override
     @Transactional(readOnly = true)
@@ -38,29 +66,6 @@ public class MemberServiceImpl implements MemberService{
     }
 
     @Override
-    public MemberCreateResponseDto createMember(MemberCreateRequestDto requestDto) {
-        Member member = requestDto.toEntity(passwordEncoder);
-        return new MemberCreateResponseDto(memberRepository.save(member).getId());
-    }
-
-
-    @Override
-    public void deleteMember(Long id) {
-        Member member = memberRepository.findById(id)
-                .orElseThrow( () -> new IllegalArgumentException("해당 멤버가 존재하지 않습니다. ")) ;
-        memberRepository.delete(member);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public MemberReadResponseDto readMember(Long id){
-        Member member = memberRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 팀이 존재하지 않습니다."));
-
-        return new MemberReadResponseDto(member);
-    }
-
-    @Override
     public MemberUpdateResponseDto updateMember(Long id, MemberUpdateRequestDto requestDto) {
         Member member = memberRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 멤버가 존재하지 않습니다."));
@@ -69,4 +74,10 @@ public class MemberServiceImpl implements MemberService{
         return new MemberUpdateResponseDto(memberRepository.save(member).getId());
     }
 
+    @Override
+    public void deleteMember(Long id) {
+        Member member = memberRepository.findById(id)
+                .orElseThrow( () -> new IllegalArgumentException("해당 멤버가 존재하지 않습니다. ")) ;
+        memberRepository.delete(member);
+    }
 }
