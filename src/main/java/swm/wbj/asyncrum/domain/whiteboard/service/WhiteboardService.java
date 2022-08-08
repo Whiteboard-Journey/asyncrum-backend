d06edd969abe85efc39cf07b8815dff0d339ce8c
Kevin Park <wlghks0314@gmail.com>
2022-08-08 23:55:06 +0900

    [feature] wbj-399 api update to whiteboard fetch by scope

--- src/main/java/swm/wbj/asyncrum/domain/whiteboard/service/WhiteboardService.java
+++ src/main/java/swm/wbj/asyncrum/domain/whiteboard/service/WhiteboardService.java
@@ -1,6 +1,7 @@
 package swm.wbj.asyncrum.domain.whiteboard.service;
 
 import swm.wbj.asyncrum.domain.whiteboard.dto.*;
+import swm.wbj.asyncrum.global.type.ScopeType;
 
 import java.io.IOException;
 
@@ -13,7 +14,7 @@ public interface WhiteboardService {
     WhiteboardReadResponseDto readWhiteboard(Long id) throws IOException;
 
     // 화이트보드 문서 전체 조희
-    WhiteboardReadAllResponseDto readAllWhiteboard(Integer pageIndex, Long topId);
+    WhiteboardReadAllResponseDto readAllWhiteboard(ScopeType scope, Integer pageIndex, Long topId);
 
     // 화이트보드 문서 정보 업데이트
     WhiteboardUpdateResponseDto updateWhiteboard(Long id, WhiteboardUpdateRequestDto requestDto) throws IOException;
