d5b22e992d6b8a8232bf32cc87e71d8de75ce1af
LeeJin0527 <ijin17463@gmail.com>
2022-08-09 13:22:30 +0900

    [feature/wbj-400-record-scope] 녹화 범위 기반 API 재구성

--- src/main/java/swm/wbj/asyncrum/domain/record/service/RecordService.java
+++ src/main/java/swm/wbj/asyncrum/domain/record/service/RecordService.java
@@ -1,15 +1,14 @@
 package swm.wbj.asyncrum.domain.record.service;
 
 import swm.wbj.asyncrum.domain.record.dto.*;
+import swm.wbj.asyncrum.global.type.ScopeType;
 
 import java.io.IOException;
 
 public interface RecordService {
     RecordCreateResponseDto createRecord(RecordCreateRequestDto requestDto) throws IOException;
     RecordReadResponseDto readRecord(Long id) throws Exception;
-    RecordReadAllResponseDto readAllRecord(Integer pageIndex, Long topId);
+    RecordReadAllResponseDto readAllRecord(ScopeType scope, Integer pageIndex, Long topId);
     RecordUpdateResponseDto updateRecord(Long id, RecordUpdateRequestDto requestDto) throws IOException;
     void deleteRecord(Long id);
-    RecordReadDailyResponseDto readDailyRecord(Long topId, Long teamId);
-
 }
