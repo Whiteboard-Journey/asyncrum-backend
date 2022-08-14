0bdce93caa60ed8d85041b5718d578ae7aaafd06
LeeJin0527 <ijin17463@gmail.com>
2022-08-14 18:34:28 +0900

    [feature/wbj-398-fullname-initia] 초기 fullname 이니셜 주소값 추가 구현

--- src/main/java/swm/wbj/asyncrum/domain/userteam/member/dto/MemberCreateRequestDto.java
+++ src/main/java/swm/wbj/asyncrum/domain/userteam/member/dto/MemberCreateRequestDto.java
@@ -22,7 +22,33 @@ public class MemberCreateRequestDto {
                 .password(passwordEncoder.encode(password))
                 .fullname(fullname)
                 .roleType(RoleType.GUEST)
+                .profileImageUrl(getProfileImageUrl(fullname))
                 .build();
     }
 
+    private String getProfileImageUrl(String fullname) {
+        return "https://ui-avatars.com/api/?name=" + tokenize(fullname);
+    }
+
+    /*
+      1. space를 + 로 치환
+      2. 대문자 앞에 + 추가. 단, 맨 앞글자는 제외
+    */
+    private String tokenize(String fullname) {
+        StringBuilder result = new StringBuilder("" + fullname.charAt(0));
+        for (int i=1; i< fullname.length(); i++) {
+            char c = fullname.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                result.append("+");
+                result.append(c);
+            }
+            else if (c == ' ') {
+                result.append("+");
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
 }
