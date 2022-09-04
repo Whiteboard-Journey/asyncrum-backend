2a44fd93867e3e57a1a9d256cbd2398f82369e87
LeeJin0527 <ijin17463@gmail.com>
2022-09-04 22:09:04 +0900

    [refactor/whiteboard-domain] whiteboard도메인 리펙토링

--- src/main/java/swm/wbj/asyncrum/domain/whiteboard/dto/WhiteboardUpdateRequestDto.java
+++ src/main/java/swm/wbj/asyncrum/domain/whiteboard/dto/WhiteboardUpdateRequestDto.java
@@ -3,9 +3,12 @@ package swm.wbj.asyncrum.domain.whiteboard.dto;
 import lombok.Data;
 import swm.wbj.asyncrum.global.type.ScopeType;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class WhiteboardUpdateRequestDto {
 
+    @NotNull
     private String title;
     private String description;
     private String scope;
