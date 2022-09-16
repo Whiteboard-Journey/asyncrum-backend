b4ff0660198983e7cd8a80bc9cd83e902c0d5561
Komputer-P <wlghks0314@gmail.com>
2022-09-16 22:27:34 +0900

    [refactor] additional refactor with bug fix

--- src/main/java/swm/wbj/asyncrum/domain/whiteboard/dto/WhiteboardReadAllResponseDto.java
+++ src/main/java/swm/wbj/asyncrum/domain/whiteboard/dto/WhiteboardReadAllResponseDto.java
@@ -1,17 +1,24 @@
 package swm.wbj.asyncrum.domain.whiteboard.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.domain.Pageable;
 import swm.wbj.asyncrum.domain.whiteboard.entity.Whiteboard;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
-@AllArgsConstructor
 public class WhiteboardReadAllResponseDto {
 
-    private List<Whiteboard> whiteboards;
+    private List<WhiteboardReadResponseDto> whiteboards;
     private Pageable pageable;
     private Boolean isList;
+
+    public WhiteboardReadAllResponseDto(List<Whiteboard> whiteboards, Pageable pageable, Boolean isList) {
+        this.whiteboards = whiteboards.stream()
+                .map(WhiteboardReadResponseDto::new)
+                .collect(Collectors.toList());
+        this.pageable = pageable;
+        this.isList = isList;
+    }
 }
--- src/main/java/swm/wbj/asyncrum/domain/whiteboard/dto/WhiteboardReadResponseDto.java
+++ src/main/java/swm/wbj/asyncrum/domain/whiteboard/dto/WhiteboardReadResponseDto.java
@@ -1,6 +1,7 @@
 package swm.wbj.asyncrum.domain.whiteboard.dto;
 
 import lombok.Data;
+import swm.wbj.asyncrum.domain.userteam.member.dto.MemberReadResponseDto;
 import swm.wbj.asyncrum.domain.whiteboard.entity.Whiteboard;
 import swm.wbj.asyncrum.global.type.ScopeType;
 
@@ -11,11 +12,13 @@ public class WhiteboardReadResponseDto {
     private String description;
     private String whiteboardUrl;
     private ScopeType scope;
+    private MemberReadResponseDto member;
 
     public WhiteboardReadResponseDto(Whiteboard whiteboard) {
         this.title = whiteboard.getTitle();
         this.description = whiteboard.getDescription();
         this.whiteboardUrl = whiteboard.getWhiteboardFileUrl();
         this.scope = whiteboard.getScope();
+        this.member = new MemberReadResponseDto(whiteboard.getMember());
     }
 }
