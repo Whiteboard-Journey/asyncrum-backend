bc0446391e67858d34b179effaa1f001824056b6
Kevin Park <wlghks0314@gmail.com>
2022-09-23 18:27:01 +0900

    [feature/refactor] update record api & refactor record/whiteboard domain

--- src/main/java/swm/wbj/asyncrum/domain/record/dto/RecordReadResponseDto.java
+++ src/main/java/swm/wbj/asyncrum/domain/record/dto/RecordReadResponseDto.java
@@ -2,6 +2,7 @@ package swm.wbj.asyncrum.domain.record.dto;
 
 import lombok.Data;
 import swm.wbj.asyncrum.domain.record.entity.Record;
+import swm.wbj.asyncrum.domain.userteam.member.dto.MemberReadResponseDto;
 import swm.wbj.asyncrum.global.type.ScopeType;
 
 @Data
@@ -13,11 +14,14 @@ public class RecordReadResponseDto {
     private String projectMetadata;
     private ScopeType scope;
 
+    private MemberReadResponseDto member;
+
     public RecordReadResponseDto(Record record){
         this.title = record.getTitle();
         this.description = record.getDescription();
         this.recordUrl = record.getRecordFileUrl();
         this.scope = record.getScope();
         this.projectMetadata = record.getProjectMetadata();
+        this.member = new MemberReadResponseDto(record.getMember());
     }
 }
--- src/main/java/swm/wbj/asyncrum/domain/record/service/RecordServiceImpl.java
+++ src/main/java/swm/wbj/asyncrum/domain/record/service/RecordServiceImpl.java
@@ -37,14 +37,13 @@ public class RecordServiceImpl implements RecordService {
     @Override
     public RecordCreateResponseDto createRecord(RecordCreateRequestDto requestDto) {
         Member currentMember = memberService.getCurrentMember();
-        Team currentTeam = teamService.getTeamWithTeamMemberValidation(requestDto.getTeamId(), currentMember);
+        Team currentTeam = validateRecordTeamMember(requestDto.getTeamId(), currentMember);
 
         Record record = requestDto.toEntity(currentMember, currentTeam);
         Long recordId = recordRepository.save(record).getId();
 
-        String recordFileKey = createRecordFileKey(currentMember.getId(), recordId);
+        String recordFileKey = createRecordFileKey(currentTeam.getId(), currentMember.getId(), recordId);
         String preSignedURL = awsService.generatePresignedURL(recordFileKey, RECORD_BUCKET_NAME, FileType.MP4);
-
         record.updateRecordFileMetadata(recordFileKey, awsService.getObjectURL(recordFileKey, RECORD_BUCKET_NAME));
 
         return new RecordCreateResponseDto(record.getId(), preSignedURL);
@@ -52,22 +51,23 @@ public class RecordServiceImpl implements RecordService {
 
     @Override
     @Transactional(readOnly = true)
-    public RecordReadAllResponseDto readAllRecord(Long teamId, ScopeType scope, Integer pageIndex, Long topId, Integer sizePerPage) {
+    public RecordReadAllResponseDto readAllRecord(Long teamId, ScopeType scope, Integer pageIndex,
+                                                  Long topId, Integer sizePerPage) {
         Member currentMember = memberService.getCurrentMember();
-        Team currentTeam = teamService.getTeamWithTeamMemberValidation(teamId, currentMember);
+        Team currentTeam = validateRecordTeamMember(teamId, currentMember);
 
         Page<Record> recordPage;
         Pageable pageable = PageRequest.of(pageIndex, sizePerPage, Sort.Direction.DESC, "id");
 
         if(isTeamScope(scope)) {
             recordPage = (topId == 0L) ?
-                    recordRepository.findAllByTeam(currentTeam, pageable) :
-                    recordRepository.findAllByTeamAndTopId(currentTeam.getId(), currentMember.getId(), topId, pageable);
+                    recordRepository.findAllByTeam(currentTeam, currentMember, pageable) :
+                    recordRepository.findAllByTeamAndTopId(currentTeam, currentMember, topId, pageable);
         }
         else {
             recordPage = (topId == 0L) ?
                     recordRepository.findAllByTeamAndMember(currentTeam, currentMember, pageable) :
-                    recordRepository.findAllByTeamAndMemberAndTopId(currentTeam.getId(), currentMember.getId(), topId, pageable);
+                    recordRepository.findAllByTeamAndMemberAndTopId(currentTeam, currentMember, topId, pageable);
         }
 
         return new RecordReadAllResponseDto(recordPage.getContent(), recordPage.getPageable(), recordPage.isLast());
@@ -80,22 +80,14 @@ public class RecordServiceImpl implements RecordService {
         Record record = recordRepository.findById(id)
                 .orElseThrow(RecordNotExistsException::new);
 
-        if (!ownsRecord(currentMember, record)) {
-            throw new OperationNotAllowedException();
-        }
+        validateRecordTeamMember(record.getTeam().getId(), currentMember);
 
         return new RecordReadResponseDto(record);
     }
 
     @Override
     public RecordUpdateResponseDto updateRecord(Long id, RecordUpdateRequestDto requestDto) {
-        Member currentMember = memberService.getCurrentMember();
-        Record record = recordRepository.findById(id)
-                .orElseThrow(RecordNotExistsException::new);
-
-        if (!ownsRecord(currentMember, record)) {
-            throw new OperationNotAllowedException();
-        }
+        Record record = getMemberRecord(id);
 
         record.updateTitleAndDescription(requestDto.getTitle(), requestDto.getDescription());
         record.updateRecordProjectMetadata(requestDto.getProjectMetadata());
@@ -109,6 +101,17 @@ public class RecordServiceImpl implements RecordService {
 
     @Override
     public void deleteRecord(Long id) {
+        Record record = getMemberRecord(id);
+
+        awsService.deleteFile(record.getRecordFileKey(), RECORD_BUCKET_NAME);
+        recordRepository.delete(record);
+    }
+
+    private Team validateRecordTeamMember(Long teamId, Member currentMember) {
+        return teamService.getTeamWithTeamMemberValidation(teamId, currentMember);
+    }
+
+    private Record getMemberRecord(Long id) {
         Member currentMember = memberService.getCurrentMember();
         Record record = recordRepository.findById(id)
                 .orElseThrow(RecordNotExistsException::new);
@@ -117,15 +120,14 @@ public class RecordServiceImpl implements RecordService {
             throw new OperationNotAllowedException();
         }
 
-        awsService.deleteFile(record.getRecordFileKey(), RECORD_BUCKET_NAME);
-        recordRepository.delete(record);
-    }
-
-    private String createRecordFileKey(Long memberId, Long recordId) {
-        return RECORD_FILE_PREFIX + "_" + memberId + "_" + recordId + "." + FileType.MP4.getName();
+        return record;
     }
 
     private boolean ownsRecord(Member currentMember, Record record) {
         return record.getMember().equals(currentMember);
+    }
+
+    private String createRecordFileKey(Long teamId, Long memberId, Long recordId) {
+        return RECORD_FILE_PREFIX + "_" + teamId + "_" + memberId + "_" + recordId + "." + FileType.MP4.getName();
     }
 }
