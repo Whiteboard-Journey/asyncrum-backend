12804ee6f3bceff4a4f942333446676c44c30c70
JIN <zinzun@knu.ac.kr>
2022-11-09 21:43:21 +0900

    [feature/meeting-service] WBJ-966 화상회의 및 녹화 API 및 단위 테스트 작성

---
+++ src/main/java/swm/wbj/asyncrum/domain/team/meeting/exception/MeetingNameAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package swm.wbj.asyncrum.domain.team.meeting.exception;
+
+public class MeetingNameAlreadyExistsException extends RuntimeException{
+    public MeetingNameAlreadyExistsException(){
+        super("해당 방의 이름은 이미 있습니다. 다른 이름을 설정해주세요");
+    }
+
+    public MeetingNameAlreadyExistsException(String message){
+        super(message);
+    }
+}
